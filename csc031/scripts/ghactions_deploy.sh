bebbd3d9e6717dda3d64b5927f0ab66a272b9b89
Andrew McNaughton <mcnaughtonadm@gmail.com>
2024-07-29 14:15:16 -0700

    Update ghactions_deploy.sh

--- csc031/scripts/ghactions_deploy.sh
+++ csc031/scripts/ghactions_deploy.sh
@@ -34,22 +34,17 @@ fi
 # Generate a snapshot report on the deployment branch.
 snapshot_output="snapshot_report.html"
 git checkout "${deployment}"
-echo "Generating snapshot report '${snapshot_output}'
+echo "Generating snapshot report '${snapshot_output}'"
 memote report snapshot --filename="${snapshot_output}"
 
 # Generate the history report on the deployment branch.
-output="history_report.html"
+history_output="history_report.html"
 git checkout "${deployment}"
-echo "Generating updated history report '${output}'."
+echo "Generating updated history report '${history_output}'."
 memote report history --filename="${output}"
 
-# Check if the report file exists
-if [ -f "${output}" ]; then
-  git add "${output}"
-  git commit -m "Github actions report # ${GITHUB_SHA}"
-  git push --quiet "https://github.com/${GITHUB_REPOSITORY}.git" "${deployment}" > /dev/null
-  echo "Memote report was generated at https://pnnl-predictive-phenomics/${GITHUB_REPOSITORY}"
-else
-  echo "Error: Report file not found!"
-  exit 1
-fi
+git add "${output}"
+git add "${snapshot_output}"
+git commit -m "Github actions report ${GITHUB_SHA}"
+git push --quiet "https://github.com/${GITHUB_REPOSITORY}.git" "${deployment}" > /dev/null
+echo "Memote report was generated at https://pnnl-predictive-phenomics/${GITHUB_REPOSITORY}"
